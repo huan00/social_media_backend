ccc76b0480964cb9e9a6820cac05305e4a7a8222
huan00 <96999144+huan00@users.noreply.github.com>
2022-09-08 21:51:44 -0400

    enable process.env.port

--- controllers/post.js
+++ controllers/post.js
@@ -97,7 +97,6 @@ export const getSearchPosts = async (req, res) => {
     const findPosts = await PostMessage.find({
       $or: [{ title }, { tags: { $in: tags.split(',') } }]
     })
-    console.log(findPosts)
     res.json(findPosts)
   } catch (error) {
     res.status(404).json({ message: 'No feeds found' })
